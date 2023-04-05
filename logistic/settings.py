b619bc7a5c91bdddeb9e900ed6999a3dc004dd54
prajjwalz1 <acharyaprajjwol152@gmail.com>
2023-04-05 10:56:20 +0545

    clean templates final2

--- logistic/settings.py
+++ logistic/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATICFILES_DIR=[BASE_DIR/'media']
+STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/app/static'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
-
 
 
 # Default primary key field type
