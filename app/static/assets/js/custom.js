70c74e7c4dbb8e55da645b587d10b0ac7de6f61f
prajjwalz1 <acharyaprajjwol152@gmail.com>
2023-04-05 20:37:04 +0545

    new design with autoscrolling scrolling added

--- app/static/assets/js/custom.js
+++ app/static/assets/js/custom.js
@@ -1,10 +1,2 @@
 
 
-// Get the button that triggers the modal
-$(document).ready(function() {
-  $('#scrollBtn').click(function() {
-    $('html, body').animate({
-      scrollTop: $('#myDiv').offset().top
-    }, 1000);
-  });
-});
